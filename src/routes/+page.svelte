e1c289b5ee91b86e0bc81be5bddb30b966d93e49
Singularity Software <124126097+singularitysoft@users.noreply.github.com>
2023-02-04 11:19:19 -0500

    Update +page.svelte
    chatgpt3 generated html & css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,53 @@
-<h1>Chesshub</h1>
+<div class="chess-board">
+  <div class="row">
+    <div class="square black"></div>
+    <div class="square white"></div>
+    <div class="square black"></div>
+    <div class="square white"></div>
+    <div class="square black"></div>
+    <div class="square white"></div>
+    <div class="square black"></div>
+    <div class="square white"></div>
+    <div class="square black"></div>
+  </div>
+  <div class="row">
+    <div class="square white"></div>
+    <div class="square black"></div>
+    <div class="square white"></div>
+    <div class="square black"></div>
+    <div class="square white"></div>
+    <div class="square black"></div>
+    <div class="square white"></div>
+    <div class="square black"></div>
+    <div class="square white"></div>
+  </div>
+  <!-- Repeat the above pattern for 8 rows to create the chess board -->
+</div>
+
+<style>
+  .chess-board {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.row {
+  display: flex;
+  width: 100%;
+}
+
+.square {
+  width: 12.5%;
+  height: 0;
+  padding-bottom: 12.5%;
+  box-sizing: border-box;
+  float: left;
+}
+
+.black {
+  background-color: #8B5E3C;
+}
+
+.white {
+  background-color: #F0D9B5;
+}
+</style>
